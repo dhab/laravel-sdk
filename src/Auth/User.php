3144024d70147d99cce22ce92111aa02bbc6bfb1
Dennis Holmen <dennis.holmen@dreamhack.se>
2016-12-15 08:30:34 +0100

    Made user props private and added accessors.

--- src/Auth/User.php
+++ src/Auth/User.php
@@ -7,12 +7,22 @@ use \Illuminate\Http\Request;
 
 class User {
 	private $logged_in = false;
-	public $id;
-	public $name;
-	public $email;
+	private $id;
+	private $name;
+	private $email;
+	public function getId() {
+		return $this->id;
+	}
+	public function getName() {
+		return $this->name;
+	}
+	public function getEmail() {
+		return $this->email;
+	}
 	public function __construct(Request $request) {
 		$user = json_decode($request->headers->get('Solid-Authorization'), true);
 		if($user && $user['id']) {
+			$this->logged_in = true;
 			$this->id = $user['id'];
 			$this->name = $user['name'];
 			$this->email = $user['email'];
