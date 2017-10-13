1688acf570ba8a4c05151705f0008efda2258536
Dennis Holmen <dennis.holmen@dreamhack.se>
2017-10-13 11:39:05 +0200

    Added CI

--- check.sh
+++ check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-vendor/bin/phpcs --standard=PSR2 --report-full --report-source --report-summary src/
+vendor/bin/phpcs -s --standard=phpcs.xml --report-full --report-source --report-summary .
 vendor/bin/phpunit --coverage-text --colors=never
