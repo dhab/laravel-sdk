617ef42d05b7cf2025873e2b12c03cf68eb81b3a
Alexander Rigbo <alexander.rigbo@dreamhack.com>
2018-06-25 15:02:46 +0200

    Initial mandrill wrapper

---
+++ src/Mail/SendTemplate.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace DreamHack\SDK\Mail;
+
+use DreamHack\SDK\Facades\Guzzle;
+use Log;
+use Mandrill;
+
+/*
+ * READ THIS BEFORE USING!
+ *
+ * The plan with this is to keep most of the settings (subject, content, from-address etc)
+ * in mandrill web UI. Only override if really needed, but should pretty much be never.
+ * Just create a new template and use that if you need.
+ *
+ * To send:
+ *
+ * $mail = new SendTemplate('id-reset-password', 'alex@dreamhack.com');
+ * $mail->setVars(["reset_key" => 'ca1b9a0eee28f872e721a9ba2591320b']);
+ * $mail->send();
+ *
+ * Override subject:
+ * $mail->subject('Hello my dudes');
+ *
+ * Set name of recipient to make it more personal:
+ * $mail->name('Alexander');
+ *
+ */
+
+class SendTemplate
+{
+    public function __construct($template, $to, $data = [])
+    {
+        $this->template = $template;
+        $this->toAddress = $to;
+        $this->vars = $data;
+    }
+
+    public function setVars($data)
+    {
+        $this->vars = $data;
+    }
+
+    public function name($name)
+    {
+        $this->toName = $name;
+    }
+
+    public function subject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    private function getTo()
+    {
+        $ret['email'] = $this->toAddress;
+        $ret['type'] = 'to';
+
+        if (isset($this->toName)) {
+            $ret['name'] = $this->toName;
+        }
+
+        return [$ret];
+    }
+
+    private function getVars()
+    {
+        $ret = [];
+        foreach ($this->vars ?? [] as $name => $content) {
+            $ret[] = [
+                "name" => $name,
+                "content" => $content,
+            ];
+        }
+        return $ret;
+    }
+
+    private function getMergeVars()
+    {
+        return [[
+            'rcpt' => $this->toAddress,
+            'vars' => $this->getVars(),
+        ]];
+    }
+
+    public function send()
+    {
+        try {
+            $mandrill = new Mandrill(config('mandrill.api_key'));
+
+            // To find out more about the format of the message-array check this one out:
+            // https://mandrillapp.com/api/docs/messages.php.html
+            $message = [];
+
+            // Format of to is an array of arrays with the keys email, name, type
+            // We're just going to use address and possibly name for one recipient at a time
+            $message['to'] = $this->getTo();
+
+            // Maybe override subject?
+            if (isset($this->subject)) {
+                $message['subject'] = $this->subject;
+            }
+
+            $message['merge_vars'] = $this->getMergeVars();
+            $message['merge_language'] = 'handlebars';
+
+            // Call mandrill API
+            $result = $mandrill->messages->sendTemplate(
+                $this->template,
+                $this->getVars(),
+                $message,
+                false, // async
+                'Main Pool' // IP-pool
+            );
+
+            if ($result[0]['status'] !== 'sent') {
+                Log::error('Failed to send via mandrill', $result);
+                return false;
+            }
+
+            return true;
+        } catch (Mandrill_Error $e) {
+            Log::error('A mandrill error occurred', $e);
+            return false;
+        }
+    }
+}
