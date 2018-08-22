0045b520b297366eb1d092467225a0e3ee6efc98
Alexander Rigbo <alexander.rigbo@dreamhack.com>
2018-08-22 08:42:30 +0200

    Add a function to lookup a bunch of dhid to their names

---
+++ src/Services/API.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DreamHack\SDK\Services;
+
+use DreamHack\SDK\Facades\Guzzle;
+use GuzzleHttp\Client;
+use Log;
+
+/**
+ *
+ *     Simple wrappers around our own API
+ *
+ */
+class API extends Client
+{
+    private static function getUrl()
+    {
+        return env('API_INTERNAL_BASE_URL', 'https://api.dreamhack.com');
+    }
+
+    /**
+     * Ask ID API for names of a bunch of users
+     */
+    public static function lookupMultipleUsers($ids)
+    {
+        $res = Guzzle::request(
+            'POST',
+            self::getUrl().'/1/identity/users/multiple/public',
+            [
+                "json" => ["ids" => $ids],
+            ]
+        );
+
+        if ($res->getStatusCode() !== 200) {
+            // Throw something?
+            return [];
+        }
+
+        return json_decode($res->getBody());
+    }
+}
