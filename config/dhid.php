b4f549490f0c8c51a0757ba33ae6418c865ac2e5
Alexander Rigbo <alexander.rigbo@dreamhack.com>
2018-10-02 09:42:31 +0200

    More config used by several services

--- config/dhid.php
+++ config/dhid.php
@@ -23,6 +23,11 @@ return [
     "api_internal_base_url" => env('API_INTERNAL_BASE_URL', 'https://api.dreamhack.com'),
 
     /**
+     * The base URL for external connections (eg. browsers) to connect to our API
+     */
+    "api_external_base_url" => env('API_EXTERNAL_BASE_URL', 'https://api.dreamhack.com'),
+
+    /**
      * Another part of the URLs for the API, the name of the service usually. eg. internal, content etc.
      */
     "api_prefix" => env('API_PREFIX'),
@@ -48,4 +53,24 @@ return [
      * This is a weird one, but it also doesn't matter. Leave it as dev even for prod.
      */
     "version" => env('VERSION', 'dev'),
+
+    /**
+     * The main url for dreamhack.com. Use this, append your path and it will work fine in dev and prod
+     */
+    "dhcom_url" => env('DHCOM_URL', 'https://dreamhack.com/'),
+
+    /**
+     * URL to the CDN
+     */
+    "filebank_url" => env('FILEBANK_URL', 'https://cdn.dreamhack.com/'),
+
+    /**
+     * The IP of the API Proxy, used to allow it to do certain things, make sure it's correct
+     */
+    "proxy_ip" => env('PROXY_IP', '127.0.0.1'),
+
+    /**
+     * Mandrill API key
+     */
+    "mandrill_api_key" => env('MANDRILL_API_KEY'),
 ];
