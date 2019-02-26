08d72af9479aad9bbb53d678e3484659ff5034d9
Alexander Rigbo <alexander.rigbo@dreamhack.com>
2019-02-26 13:58:11 +0100

    Log and ignore errors from API

--- src/Services/API.php
+++ src/Services/API.php
@@ -5,6 +5,7 @@ namespace DreamHack\SDK\Services;
 use DreamHack\SDK\Facades\Guzzle;
 use GuzzleHttp\Client;
 use DHID;
+use Log;
 
 /**
  *
@@ -135,6 +136,7 @@ class API extends Client
      */
     public static function awardAchievement($achievement_id, $user_id)
     {
+        try {
         $res = DHID::request(
             'POST',
             "/1/qvp/achievements/{$achievement_id}/award",
@@ -144,6 +146,9 @@ class API extends Client
                 ],
             ]
         );
-        // Handle errors?
+        } catch(\Exception $e) {
+            // Ignore any error that might come up, but log it.
+            Log::error('Failed to send internal request', ["exception" => $e]);
+        }
     }
 }
