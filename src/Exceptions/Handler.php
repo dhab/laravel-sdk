d1a68f35ac702d96478fafe806047020c4922a4a
Alexander Rigbo <alexander.rigbo@dreamhack.com>
2020-05-25 13:16:59 +0200

    Update Exception signatures

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace DreamHack\SDK\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -17,10 +17,10 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $e
+     * @param  \Throwable  $e
      * @return void
      */
-    public function report(Exception $e)
+    public function report(Throwable $e)
     {
         parent::report($e);
     }
@@ -29,10 +29,10 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Throwable  $e
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $e)
+    public function render($request, Throwable $e)
     {
         $message = $e->getMessage();
         $headers = [];
